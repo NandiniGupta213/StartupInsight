40bd3e86002a98a604912a551e1dd2104f3db509
Nandini Gupta <ng3557879@gmail.com>
2026-03-18 23:20:51 +0530

    fix: origin problem

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ dotenv.config({
 const app = express();
 
 app.use(cors({ 
-    origin: 'https://startupinsight-1.onrender.com/',
+    origin: 'https://startupinsight-1.onrender.com',
     credentials: true 
 }));
 
